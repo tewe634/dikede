b21094a251a41903215e42bccb3cdb1b1fece15c
werqeqwe <2533314379@qq.com>
2022-08-15 13:52:48 +0800

    完成titlt port

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,12 @@
 <template>
   <div id="app">
-    <!-- <router-view /> -->
-    <Pagination />
+    <router-view />
+    <!-- <Pagination /> -->
   </div>
 </template>
 
 <script>
-import Pagination from '@/components/Pagination'
 export default {
-  name: 'App',
-  components: { Pagination }
+  name: 'App'
 }
 </script>
